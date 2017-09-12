fb45c833e11d20fabb26149631d934cf23ee80e5
pwpiwi <pwpiwi@users.noreply.github.com>
2017-09-12 21:10:34 +0200

    also install additional (auxiliary) files (@iceman)

--- proxmark3.rb
+++ proxmark3.rb
@@ -5,7 +5,6 @@ class Proxmark3 < Formula
   sha256 "bace0dd34e35923bfd926cf0943e615a00b2588bb958a13afde3fd46cd34a821"
   head "https://github.com/proxmark/proxmark3.git"
 
-
   depends_on "automake" => :build
   depends_on "readline"
   depends_on "p7zip" => :build
@@ -21,19 +20,45 @@ class Proxmark3 < Formula
     ENV.deparallelize
 
 #    system "make", "-C", "client/hid-flasher/"
-    system "make", "clean"	  
+    system "make", "clean"      
     system "make", "all"
+    
     bin.mkpath
     bin.install "client/flasher" => "proxmark3-flasher"
 #    bin.install "client/hid-flasher/flasher" => "proxmark3-hid-flasher"
     bin.install "client/proxmark3" => "proxmark3"
     bin.install "client/fpga_compress" => "fpga_compress"
+
+    # default keys
+    bin.install "client/default_keys.dic" => "default_keys.dic"
+    bin.install "client/default_pwd.dic" => "default_pwd.dic"
+
+    # hardnested files
+    (bin/"hardnested/tables").mkpath
+    (bin/"hardnested").install "client/hardnested/bf_bench_data.bin"
+    (bin/"hardnested/tables").install Dir["client/hardnested/tables/*"]
+
+    # lua libs for proxmark3 scripts
+    (bin/"lualibs").mkpath
+    (bin/"lualibs").install Dir["client/lualibs/*"]
+
+    # lua scripts
+    (bin/"scripts").mkpath
+    (bin/"scripts").install Dir["client/scripts/*"]
+
+    # trace files for experimentations
+    (bin/"traces").mkpath
+    (bin/"traces").install Dir["traces/*"]
+    
     share.mkpath
+    # compiled firmware for flashing
     (share/"firmware").mkpath
     (share/"firmware").install "armsrc/obj/fullimage.elf" => "fullimage.elf"
     (share/"firmware").install "bootrom/obj/bootrom.elf" => "bootrom.elf"
+
+
 #    ohai "Install success! Upgrade devices on HID firmware with proxmark3-hid-flasher, or devices on more modern firmware with proxmark3-flasher."
-	ohai "Install success!  Only proxmark3-flasher (modern firmware) is available."
+    ohai "Install success!  Only proxmark3-flasher (modern firmware) is available."
     ohai "The latest bootloader and firmware binaries are ready and waiting in the current homebrew Cellar within share/firmware."
   end
 
